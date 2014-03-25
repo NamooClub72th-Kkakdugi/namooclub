30a84d6f53fd3869385e32c2fa19f181dc2692f9
kosta-19 <kosta-19@kosta-19-PC>
2014-03-25 15:25:23 +0900

    test

--- NamooClubThreeWeb/src/com/namoo/ns1/web/controller/community/ComListController.java
+++ NamooClubThreeWeb/src/com/namoo/ns1/web/controller/community/ComListController.java
@@ -1,9 +1,10 @@
 package com.namoo.ns1.web.controller.community;
 
 import javax.jws.WebService;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-@WebService("/community/comList.do")
+@WebServlet("/community/comList.do")
 public class ComListController extends HttpServlet{
 
 }
