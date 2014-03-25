87a9fb8c9f295ba5945cee7b59e2efc02f91698b
kosta-18 <kosta-18@kosta-18-PC>
2014-03-25 15:21:51 +0900

    3

---
+++ NamooClubThreeWeb/src/com/namoo/ns1/web/controller/community/ComListController.java
@@ -0,0 +1,9 @@
+package com.namoo.ns1.web.controller.community;
+
+import javax.jws.WebService;
+import javax.servlet.http.HttpServlet;
+
+@WebService("/community/comList.do")
+public class ComListController extends HttpServlet{
+
+}
