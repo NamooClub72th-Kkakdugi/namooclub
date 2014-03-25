ab271c33f227127d4c0dfe6eed30fcb79441e778
kosta-18 <kosta-18@kosta-18-PC>
2014-03-25 15:30:41 +0900

    885

--- NamooClubThreeWeb/src/com/namoo/ns1/web/controller/community/ComListController.java
+++ NamooClubThreeWeb/src/com/namoo/ns1/web/controller/community/ComListController.java
@@ -1,31 +1,10 @@
 package com.namoo.ns1.web.controller.community;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+import javax.jws.WebService;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/community/comList.do")
 public class ComListController extends HttpServlet{
-
-	private static final long serialVersionUID = 2015998138536728657L;
-
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//
-		doPost(req, resp);
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//
-		
-	}
-	
-	
 
 }
