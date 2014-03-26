70a7e8670e751ca2038d01a07bb5e565db69bd4e
kosta-19 <kosta-19@kosta-19-PC>
2014-03-26 16:14:15 +0900

    콤조인인풋.jsp 되어라

--- NamooClubThreeWeb/WebContent/WEB-INF/views/club/clubCreateCheck.jsp
+++ NamooClubThreeWeb/WebContent/WEB-INF/views/club/clubCreateCheck.jsp
@@ -139,25 +139,7 @@
 				</div>
 			</div>
 		</div>
-
-		<!-- Footer ========================================================================================== -->
-		<footer>
-		<div class="row">
-			<div class="col-lg-12">
-				<ul class="list-unstyled">
-					<li class="pull-right"><a href="#top">위로 이동</a></li>
-					<li><a href="#">커뮤니티 홈</a></li>
-					<li><a href="#">RSS</a></li>
-					<li><a href="#">이용약관</a></li>
-					<li><a href="#">도움말</a></li>
-					<li><a href="#">회원탈퇴</a></li>
-				</ul>
-				<p>© NamooSori 2014.</p>
-			</div>
-		</div>
-		</footer>
 	</div>
-
 	<%@ include file="/WEB-INF/views/common/footer.jsp"%>
 </body>
 </html>
---
+++ NamooClubThreeWeb/src/com/namoo/ns1/web/controller/club/ClubCreateController.java
@@ -0,0 +1,47 @@
+package com.namoo.ns1.web.controller.club;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.namoo.ns1.service.facade.CommunityService;
+import com.namoo.ns1.service.factory.NamooClubServiceFactory;
+
+import dom.entity.SocialPerson;
+
+public class ClubCreateController extends HttpServlet{
+
+	private static final long serialVersionUID = 4827189201539721388L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//
+//		SocialPerson person = (SocialPerson) req.getSession().getAttribute("loginUser");
+//		String email = person.getEmail();
+		
+		String clubCategory = req.getParameter("clubCategory");
+		String clubName = req.getParameter("clubName");
+		String clubDescription = req.getParameter("clubDescription");
+		String clubQuOne = req.getParameter("clubQuOne");
+		String clubQuTwo = req.getParameter("clubQuTwo");
+		String clubQuThree = req.getParameter("clubQuThree");
+		
+//		CommunityService service = NamooClubServiceFactory.getInstance().getCommunityService();
+//		service.
+		
+//		redirect(req, resp, "/community/comList.do");
+	}
+	
+	
+
+
+}
