46686a8eb38d364984ab111e16a16b37189a394e
kosta-19 <kosta-19@kosta-19-PC>
2014-03-27 14:50:55 +0900

    qkedk

--- NamooClubThreeWeb/src/com/namoo/ns1/web/controller/club/ClubCreateController.java
+++ NamooClubThreeWeb/src/com/namoo/ns1/web/controller/club/ClubCreateController.java
@@ -4,39 +4,33 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.namoo.ns1.service.facade.ClubService;
 import com.namoo.ns1.service.factory.NamooClubServiceFactory;
+import com.namoo.ns1.web.controller.shared.DefaultController;
+
+import dom.entity.SocialPerson;
 
 @WebServlet("/club/clubCreate.do")
-public class ClubCreateController extends HttpServlet{
+public class ClubCreateController extends DefaultController{
 
 	private static final long serialVersionUID = 4827189201539721388L;
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 
-		doPost(req, resp);
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//
 		ClubService service = NamooClubServiceFactory.getInstance().getClubService();
+		SocialPerson person = (SocialPerson) req.getSession().getAttribute("loginUser");
+		String email = person.getEmail();
 		
 		
 		String clubCategory = req.getParameter("clubCategory");
 		String clubName = req.getParameter("clubName");
 		String clubDescription = req.getParameter("clubDescription");
-		String clubQuOne = req.getParameter("clubQuOne");
-		String clubQuTwo = req.getParameter("clubQuTwo");
-		String clubQuThree = req.getParameter("clubQuThree");
-
-	
 		
-		
+		service.registClub(clubCategory, clubName, clubDescription, email);
+		redirect(req, resp, "/club/clubList.do");
 	}
 }
